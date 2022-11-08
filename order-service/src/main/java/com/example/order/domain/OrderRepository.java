9725aea7610169436dcf9860e6a6e9f01f43d351
Lee <euideok@bcd0746ba77d.ant.amazon.com>
2022-11-08 16:56:38 +0900

    modify customer

--- order-service/src/main/java/com/example/order/domain/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.order.domain;
-
-import com.example.common.domain.OrderDetail;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<OrderDetail, Long> {
-}
